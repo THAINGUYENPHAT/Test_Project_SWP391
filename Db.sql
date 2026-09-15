5769045c78d4d7175c53fb4ef189fac92bf10a1d
Thai Nguyen Phat <phattnce200188@gmail.com>
2026-09-15 09:39:50 +0700

    Initial Commit

---
+++ Db.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE Test_SWP391_Group3;
+GO
+USE Test_SWP391_Group3;
+
+CREATE TABLE [Users] (
+    userID INT PRIMARY KEY IDENTITY(1,1),
+    username VARCHAR(50) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    role VARCHAR(20) NOT NULL
+);
